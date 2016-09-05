79de32c0ff827aae0b61713ee6a71b507d78f5c2
Vaclav Maixner <vaclavmaixnerj@gmail.com>
2016-09-05 12:59:38 +0200

    Create Maven project.

--- matfyz/src/matfyz/Main.java
+++ matfyz/src/matfyz/Main.java
@@ -1,5 +1,10 @@
 package matfyz;
 
 public class Main {
-
+	private double kp = 1; // konstanty proporcni, integracni, derivacni
+	private double ki = 1;
+	private double kd = 1;
+	
+	Pid pid1 = new Pid();
+	pid1.solve(kp, ki, kd);
 }
--- matfyz/src/matfyz/Pid.java
+++ matfyz/src/matfyz/Pid.java
@@ -4,10 +4,33 @@ public class Pid {
 	private double Input; // merena aktualni hodnota
 	private double Output; // vysledek, vydana hodnota po regulaci
 	private double Setpoint; // setpoint je pozadovana, referencni hodnota
+	private double errorSum;
+	private double time;
+	private double deltaTime;
+	private double previousError;
 
-	@Override
-	public String toString() {
+	public double solve(double kp, double ki, double kd) {
+		// inicializace
+		deltaTime = 0.1;
+		time = 0;
 
-		return null;
+		// Spocitat error
+		double error = (Setpoint - Input);
+
+		// Pricist error do error sum
+		errorSum += (error * deltaTime);
+
+		// zavest derivacni error
+		double derError = (error - previousError) / deltaTime;
+
+		// Spocitat vysledek PID
+		Output = kp * error + ki * errorSum + kd * derError;
+
+		// pripravit na dalsi kolo
+		previousError = error;
+
+		// time meri ubehnuty cas
+		time += deltaTime;
+		return Output;
 	}
 }
